f15e8f3957df3e54f4751759f1bc6d1fbacfe8ea
Cristian Mendoza <mendozawebsites@gmail.com>
2017-10-26 16:08:24 -0700

    More mobile friendly media queries v3

---
+++ java/price-plans.js
@@ -0,0 +1,21 @@
+$(document).ready( function(){  
+   $("#click-below").delay(4000).fadeOut(1500); 
+});
+$("#price-logo-1").click( function(){
+   $("#price-content-1").slideToggle(1000);
+   $("#price-content-2").slideUp(1000);
+   $("#price-content-3").slideUp(1000);
+
+});
+
+$("#price-logo-2").click( function(){
+   $("#price-content-2").slideToggle(1000);
+   $("#price-content-1").slideUp(1000);
+});
+
+$("#price-logo-3").click( function(){
+   $("#price-content-3").slideToggle(1000);
+       $("#price-content-2").slideUp(1000);
+
+});
+
