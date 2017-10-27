6e9ee469688d1429c387f7f36f871d49b141b291
Cristian Mendoza <mendozawebsites@gmail.com>
2017-10-26 19:34:12 -0700

    Mobile friendly MQ v5

--- java/price-plans.js
+++ java/price-plans.js
@@ -11,11 +11,13 @@ $("#price-logo-1").click( function(){
 $("#price-logo-2").click( function(){
    $("#price-content-2").slideToggle(1000);
    $("#price-content-1").slideUp(1000);
+    $("#price-content-3").slideUp(1000);
 });
 
 $("#price-logo-3").click( function(){
    $("#price-content-3").slideToggle(1000);
        $("#price-content-2").slideUp(1000);
+        $("#price-content-1").slideUp(1000);
 
 });
 
